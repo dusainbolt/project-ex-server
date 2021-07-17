04044727accae97dcaecbdef8cd06af5fd54490d
kai <dulh181199@gmail.com>
2021-07-17 20:09:28 +0700

    fix

--- app/models/project.model.js
+++ app/models/project.model.js
@@ -1,34 +1,33 @@
 module.exports = (sequelize, Sequelize) => {
-    const Project = sequelize.define('project', {
-        title: {
-            type: Sequelize.STRING,
-        },
-        description: {
-            type: Sequelize.STRING,
-        },
-        teacher: {
-            type: Sequelize.STRING,
-        },
-        nameStudent: {
-            type: Sequelize.STRING,
-        },
-        codeStudent: {
-            type: Sequelize.STRING,
-        },
-        avatarStudent: {
-            type: Sequelize.STRING,
-        },
-        classStudent: {
-            type: Sequelize.STRING,
-        },
-        file: {
-            type: Sequelize.STRING,
-        },
-        type: {
-            type: Sequelize.INTEGER,
-            default: _constant.STATUS_ACTIVE,
-        },
-    });
+  const Project = sequelize.define('project', {
+    title: {
+      type: Sequelize.STRING,
+    },
+    description: {
+      type: Sequelize.STRING,
+    },
+    teacher: {
+      type: Sequelize.STRING,
+    },
+    nameStudent: {
+      type: Sequelize.STRING,
+    },
+    codeStudent: {
+      type: Sequelize.STRING,
+    },
+    avatarStudent: {
+      type: Sequelize.STRING,
+    },
+    classStudent: {
+      type: Sequelize.STRING,
+    },
+    file: {
+      type: Sequelize.STRING,
+    },
+    type: {
+      type: Sequelize.INTEGER,
+    },
+  });
 
-    return Project;
+  return Project;
 };
